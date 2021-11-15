03b1b5ead3b87df15da82f7a6e43bfffd536d32a
ManishaAppu <mmanishamca@gmail.com>
2021-11-15 12:52:44 +0530

    Added Micro Service

---
+++ eureka/src/test/java/com/makemefly/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makemefly.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
